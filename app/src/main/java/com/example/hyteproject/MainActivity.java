899cb489e82d00c6aff418ba8a8782bed67a6e6a
morttim <mortti.malin@metropolia.fi>
2021-03-02 10:52:25 +0200

    Merged branches together. Fixed bugs that occured due to this process.

--- app/src/main/java/com/example/hyteproject/MainActivity.java
+++ app/src/main/java/com/example/hyteproject/MainActivity.java
@@ -1,14 +1,67 @@
 package com.example.hyteproject;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
 
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
-    @Override
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private TextView textViewStep;
+    private SensorManager sensorManager;
+    private Sensor stepCounter;
+    private boolean isCounterSensorPresent;
+    int stepCount = 0;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
+            //ask for permission
+            requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 0);
+        }
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        textViewStep = findViewById(R.id.stepText);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null){
+            stepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            isCounterSensorPresent = true;
+        }else {
+            textViewStep.setText("Counter not present");
+            isCounterSensorPresent = false;
+        }
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor == stepCounter){
+            stepCount = (int) sensorEvent.values[0];
+            textViewStep.setText(String.valueOf(stepCount));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+    protected void onResume() {
+        super.onResume();
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null)
+            sensorManager.registerListener(this, stepCounter, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+
 }
